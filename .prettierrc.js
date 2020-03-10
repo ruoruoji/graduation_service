819e805e5e492f4c06cc3a4b47e1429b54926330
sq <867149076@qq.com>
2020-03-10 11:22:24 +0800

    chore: 尾逗号配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "tabWidth": 2, //一个tab代表几个空格数，默认为80
-  "singleQuote": true, //字符串是否使用单引号，默认为false，使用双引号
-  "semi": true, //行位是否使用分号，默认为true
-  "trailingComma": "all", //是否使用尾逗号，有三个可选值"<none|es5|all>"
-  "bracketSpacing": true, //对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
-}
+  tabWidth: 2, //一个tab代表几个空格数，默认为80
+  singleQuote: true, //字符串是否使用单引号，默认为false，使用双引号
+  semi: true, //行位是否使用分号，默认为true
+  trailingComma: 'es5', //是否使用尾逗号，有三个可选值"<none|es5|all>"
+  bracketSpacing: true, //对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
+};
