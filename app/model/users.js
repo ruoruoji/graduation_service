51d216393921fa728f1c9ae5b8e24c92b005303d
sq <867149076@qq.com>
2020-03-14 23:08:52 +0800

    feat(database_users): 完善数据库用户表

--- app/model/users.js
+++ app/model/users.js
@@ -3,13 +3,39 @@
 module.exports = app => {
   const { STRING, INTEGER, DATE } = app.Sequelize;
 
-  const Users = app.model.define('users', {
-    id: { type: INTEGER, primaryKey: true, autoIncrement: true },
-    account: { type: STRING(10), allowNull: false, unique: true },
-    password: { type: STRING(30), allowNull: false },
-    created_at: DATE,
-    updated_at: DATE
-  });
+  const Users = app.model.define(
+    'users',
+    {
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        comment: '主键'
+      },
+      account: {
+        type: STRING,
+        allowNull: false,
+        unique: true,
+        comment: '工号'
+      },
+      password: { type: STRING, allowNull: false, comment: '密码' },
+      role: { type: STRING, comment: '角色' },
+      name: { type: STRING, comment: '姓名' },
+      age: { type: INTEGER, comment: '年龄' },
+      department: { type: INTEGER, comment: '部门' },
+      title: { type: STRING, comment: '职称' },
+      address: { type: STRING, comment: '联系地址' },
+      phone_num: { type: STRING, comment: '手机号码' },
+      email: { type: STRING, comment: '电子邮件' },
+      introduce: { type: STRING, comment: '个人简介' },
+      avatar: { type: STRING, comment: '头像' },
+      created_at: DATE,
+      updated_at: DATE
+    },
+    {
+      comment: '用户表'
+    }
+  );
 
   Users.findByAccount = async account => {
     return await Users.findOne({
--- database/migrations/20200220133202-init-users.js
+++ database/migrations/20200220133202-init-users.js
@@ -4,10 +4,31 @@ module.exports = {
   // 在执行数据库升级时调用的函数，创建 users 表
   up: async (queryInterface, Sequelize) => {
     const { INTEGER, DATE, STRING } = Sequelize;
+
     await queryInterface.createTable('users', {
-      id: { type: INTEGER, primaryKey: true, autoIncrement: true },
-      account: { type: STRING(10), allowNull: false, unique: true },
-      password: { type: STRING(30), allowNull: false },
+      id: {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        comment: '主键'
+      },
+      account: {
+        type: STRING,
+        allowNull: false,
+        unique: true,
+        comment: '工号'
+      },
+      password: { type: STRING, allowNull: false, comment: '密码' },
+      role: { type: STRING, comment: '角色' },
+      name: { type: STRING, comment: '姓名' },
+      age: { type: INTEGER, comment: '年龄' },
+      department: { type: STRING, comment: '部门' },
+      title: { type: STRING, comment: '职称' },
+      address: { type: STRING, comment: '联系地址' },
+      phone_num: { type: STRING, comment: '手机号码' },
+      email: { type: STRING, comment: '电子邮件' },
+      introduce: { type: STRING, comment: '个人简介' },
+      avatar: { type: STRING, comment: '头像' },
       created_at: DATE,
       updated_at: DATE
     });
